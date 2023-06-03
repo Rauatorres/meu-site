367d922d3241c267de83b332a5bc1faa644a7e38
rauatorres <rauatorres@gmail.com>
2023-06-02 21:48:14 -0300

    deletando pastas inutilizadas

--- client/src/components/content/sobre/apresentacao/Descricao.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react-dom'
-
-export default props=>
-<p className='ApresentacaoDescricao'>
-  Sou um programador Fullstack. Programo tanto a parte visual e interativa do site no lado do cliente, quanto a interação do sistema com o servidor com manipulação e armazenamento de dados. Utilizo tecnologias JavaScript como React e NodeJs.
-</p>
