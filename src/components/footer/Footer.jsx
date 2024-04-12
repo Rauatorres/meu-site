b30d31443eca73b2e33fa75ef5b7486a61970ff4
rauatorres <rauatorres@gmail.com>
2024-04-12 13:57:38 -0300

    finalizando footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 
+import { FaReact } from "react-icons/fa"
+
+import './css/Footer.css'
+
 export default props=>
 <footer>
-    Footer
+    <div className='FooterColuna FooterColunaDesenvolvedor'>
+        <div className='FooterTitulo'>Site feito por</div>
+        <div className='FooterConteudo'>Rauã Torres</div>
+    </div>
+    <div className='FooterColuna FooterColunaTecnologiasUsadas'>
+        <div className='FooterTitulo'>Feito com</div>
+        <div className='FooterConteudo'><FaReact/> <span className='FooterTecnologiaUsada'>ReactJS</span> </div>
+    </div>
+    <div className='FooterColuna FooterColunaRepositorio'>
+        <div className='FooterTitulo'>Repositório do site</div>
+        <a className='FooterConteudo FooterLinkRepositorio' target='__blank' href='https://github.com/Rauatorres/meu-site'>https://github.com/Rauatorres/meu-site</a>
+    </div>
 </footer>
