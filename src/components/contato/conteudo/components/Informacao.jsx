d9cc65319216e73c23e0f2dbe2506ef1a6a977dc
rauatorres <rauatorres@gmail.com>
2024-04-12 11:53:49 -0300

    adicionando hover no botão de copiar informação na área de contato e adicionando uma mensagem de copiado quando clicar no botão para copiar

--- src/components/contato/conteudo/components/Informacao.jsx
+++ src/components/contato/conteudo/components/Informacao.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
-export default props=>
-<div className='ContatoInformacao'>
-    <div className='ContatoInformacaoIconTextoBox'>
-        <span className='ContatoInformacaoIcone'>{props.icone}</span>
-        <span className='ContatoInformacaoTexto'>{props.texto}</span>
-    </div>
-    <div className='ContatoInformacaoBotaoCopiarBox' onClick={()=>{navigator.clipboard.writeText(props.texto)}}>
-        <ContentCopyIcon/>
-    </div>
-</div>
+export default props=>{
+    const [msgCopiadoDisplay, setMsgCopiadoDisplay] = useState('none')
+
+    return (
+        <div className='ContatoInformacao'>
+            <div className='ContatoInformacaoIconTextoBox'>
+                <span className='ContatoInformacaoIcone'>{props.icone}</span>
+                <span className='ContatoInformacaoTexto'>{props.texto}</span>
+            </div>
+            <div className='ContatoInformacaoBotaoCopiarBox' 
+                onClick={()=>{navigator.clipboard.writeText(props.texto); setMsgCopiadoDisplay('block')}}
+                onMouseLeave={()=>{setMsgCopiadoDisplay('none')}}
+            >
+                <ContentCopyIcon className='ContatoInformacaoBotaoCopiar'/>
+                <div className='MsgCopiado' style={{display: msgCopiadoDisplay}}>Copiado!</div>
+            </div>
+        </div>
+    )
+}
