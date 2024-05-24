1716b035db06e6bd8fb0881a9849953fcad1f069
rauatorres <rauatorres@gmail.com>
2024-05-24 13:56:41 -0300

    finalizando área de seleção de projetos responsiva na seção de projetos

--- src/components/projetos/conteudo/script/exibirBotoesSelecaoProjeto.jsx
+++ src/components/projetos/conteudo/script/exibirBotoesSelecaoProjeto.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 import BotaoSelecaoProjeto from '../area_projetos/BotaoSelecaoProjeto'
 
 export default (listaProjetos, projetoEmSelecao, selecionarProjeto)=>{
+    const isMobile = useMediaQuery({query: '(max-width: 768px)'})
+
+
+    function dimensaoBotao(dimensao, index){
+        if(dimensao == 'width'){
+            if (isMobile){
+                return projetoEmSelecao == index ? '22vw' : '15vw'
+            }else{
+                return projetoEmSelecao == index ? '80%' : '60%'
+            }
+
+        }else if (dimensao == 'height'){
+            if (isMobile){
+                return projetoEmSelecao == index ? '11vw' : '8vw'
+            }else{
+                return projetoEmSelecao == index ? '23%' : '17%'
+            }
+        }
+    }
+
     return listaProjetos.map((projeto, index)=>{
         return (
             <BotaoSelecaoProjeto 
                 img={projeto.imagem}
                 selecionarProjeto={selecionarProjeto}
                 projetoIndex={index}
-                width={projetoEmSelecao == index ? '80%' : '60%'}
-                height={projetoEmSelecao == index ? '23%' : '17%'}
+                width={dimensaoBotao('width', index)}
+                height={dimensaoBotao('height', index)}
             />
         )
     })
